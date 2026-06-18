4dc54124b5cb34327b15aa75a4fce6e8146f7da9
Михаил <mixaila17may2003@gmail.com>
2026-06-18 21:19:39 +0400

    fix: support forwarded headers behind gateway

--- src/Module/Organization.Product.Module.Presentation/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/Module/Organization.Product.Module.Presentation/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Organization.Product.Module.Presentation.DependencyInjection;
@@ -9,7 +10,13 @@ public static class ServiceCollectionExtensions
         this IServiceCollection serviceCollection,
         IConfiguration configuration)
     {
-        serviceCollection.AddHttp(configuration);
+        serviceCollection.AddHttp(configuration.GetSection("ForwardedHeaders"));
+
+        serviceCollection.Configure<ForwardedHeadersOptions>(options =>
+        {
+            options.KnownIPNetworks.Clear();
+            options.KnownProxies.Clear();
+        });
 
         return serviceCollection;
     }
